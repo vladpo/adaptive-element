1d078f0ece6fccfaddf984ba082e78f79604719f
Vlad Popovici <vlad.popovici@ullink.com>
2019-02-19 10:38:34 +0200

    some changes

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,18 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main where
 
 --import Graphics.Rendering.Chart.Easy
 --import Graphics.Rendering.Chart.Backend.Cairo
 import qualified System.Random    as Rand (mkStdGen)
-import           AdaptiveElement          (fig12)
-
-swap :: (a,b) -> (b,a)
-swap (x,y) = (y,x)
+import           AdaptiveElement          (fig12, d)
+import Control.Monad.State (runState)
+import Numeric.Probability.Random ( pick, decons)
+import  Numeric.Probability.Distribution ( just
+                                         , (??)
+                                         )
+import Text.Printf
 
 main :: IO ()
-main = print $ fig12 (Rand.mkStdGen 12)
+main = writeFile "/home/vpo/workspace/adaptive-element/out.csv" $ foldl (\s t -> s ++ show (snd t) ++ "," ++ printf "%.4f" (fst t) ++ "\r\n") "ITI,Lambda\r\n" (fig12 $ Rand.mkStdGen 12)
+--main = print $ runState (decons $ pick d) (Rand.mkStdGen 13)
--- src/AdaptiveElement.hs
+++ src/AdaptiveElement.hs
@@ -4,6 +4,7 @@
 
 module AdaptiveElement
     ( fig12
+    , d
     ) where
 
 import            Control.Lens                      hiding ((??))
@@ -11,6 +12,7 @@ import            Control.Monad.State               (runState)
 import            Debug.Trace                       as T (trace)
 import qualified  Numeric.Probability.Shape         as NPS ( normalCurve)
 import qualified  Numeric.Probability.Distribution  as NPD ( shape
+                                                           , T
                                                            )
 import qualified  Numeric.Probability.Random        as NPR ( pick
                                                            , decons
@@ -18,21 +20,21 @@ import qualified  Numeric.Probability.Random        as NPR ( pick
 import qualified System.Random                      as Rand (StdGen)
 import           Data.List                                 (genericLength)
 
-data Constants = C { alfa :: Double
-                   , beta :: Double
-                   , gamma :: Double
-                   , lambda :: Double
-                   , mean :: Double
-                   , deviation :: Double
+data Constants = C { alfa :: Float
+                   , beta :: Float
+                   , gamma :: Float
+                   , lambda :: Float
+                   , mean :: Float
+                   , deviation :: Float
                    }
-type Weight = Double
+type Weight = Float
 type Weights = [Weight]
-type Input = Double
+type Input = Float
 type Inputs = [Input]
-type InTrace = Double
+type InTrace = Float
 type InTraces = [InTrace]
-type Output = Double
-type OutTrace = Double
+type Output = Float
+type OutTrace = Float
 
 tmul :: Num a => (a, a) -> a
 tmul (x, y) = x*y
@@ -40,8 +42,9 @@ tmul (x, y) = x*y
 inputTrace :: Constants -> Inputs -> InTraces -> InTraces
 inputTrace env xs xts = itrace (alfa env) <$> zip xs xts
   where
-    itrace :: Double -> (Input, InTrace) -> InTrace
-    itrace alfa t = alfa * snd t + fst t
+    itrace :: Float -> (Input, InTrace) -> InTrace
+    itrace alfa t = if it >= 0.0 then it else 0.0
+      where it = alfa * snd t + fst t
 
 initWeight :: Weights
 initWeight = replicate 4 0.01
@@ -65,46 +68,52 @@ outputTrace :: Constants -> Output -> OutTrace -> OutTrace
 outputTrace env y y' = b*y' + (1 - b)*y
   where b = beta env
 
-randomNoise :: Constants -> Rand.StdGen -> [Double] -> (Double, Rand.StdGen)
+randomNoise :: Constants -> Rand.StdGen -> [Float] -> (Float, Rand.StdGen)
 randomNoise env g xs = flip runState g $ NPR.decons (NPR.pick $ NPD.shape (NPS.normalCurve m d) xs)
   where (m, d) = (mean env, deviation env)
 
-noiseValues :: [Double]
-noiseValues = map (/1000.0) [negate 100..100]
+noiseValues :: [Float]
+noiseValues = [x/1000.0| x <- [0..100]]
 
-stimulus :: Int -> Int -> Int -> [Double]
-stimulus predelay duration postdelay = concat $ replicate 20 (replicate predelay 0.0 ++ replicate duration 1.0 ++ replicate postdelay 0.0 ++ replicate 50 0.0)
+stimulus :: Int -> Int -> Int -> [Float]
+stimulus predelay duration postdelay = concat $ replicate 101 (replicate predelay 0.0 ++ replicate duration 1.0 ++ replicate postdelay 0.0 ++ replicate 50 0.0)
 
-addNoise :: Constants -> Rand.StdGen -> Double -> [Double] -> (Double, Rand.StdGen)
+addNoise :: Constants -> Rand.StdGen -> Float -> [Float] -> (Float, Rand.StdGen)
 addNoise env g v nvs = let (n, ng) = randomNoise env g nvs in (max 0.0 $ v + n, ng)
 
-conditionedStimulus :: Int -> [Double]
+conditionedStimulus :: Int -> [Float]
 conditionedStimulus iti = stimulus 1 3 (iti+30)
 
-unconditionedStimulus :: Int -> [Double]
+unconditionedStimulus :: Int -> [Float]
 unconditionedStimulus iti = stimulus (iti+1) 30 3
 
-learn :: Constants -> Weights -> [[Double]] -> InTraces -> Output -> OutTrace -> Double
-learn _ ws [] _ _ _ = sum (init ws) / 4.0
-learn env ws (xs:xss) xs' y y' = learn env ws' xss nxs' ny y
+learn :: Constants -> Weights -> Weights -> [[Float]] -> InTraces -> Output -> OutTrace -> Float
+learn _ ws mws [] _ _ _ = sum (init ws) / 4.0
+learn env ws mws (xs:xss) xs' y y' = learn env ws' (max ws mws) xss nxs' ny y
   where
     ny = output ws xs
     nxs' = inputTrace env xs xs'
-    ws' = debug $ weights env ws y y' xs'
+    ws' = weights env ws y y' xs'
 
-inputs :: Constants -> Int -> Rand.StdGen -> [Double] -> ([[Double]], Rand.StdGen)
+inputs :: Constants -> Int -> Rand.StdGen -> [Float] -> ([[Float]], Rand.StdGen)
 inputs env iti g nvs = foldr f ([], g) $ zip cs ucs
   where
     cs = conditionedStimulus iti
     ucs = unconditionedStimulus iti
-    f :: (Double, Double) -> ([[Double]], Rand.StdGen) -> ([[Double]], Rand.StdGen)
+    f :: (Float, Float) -> ([[Float]], Rand.StdGen) -> ([[Float]], Rand.StdGen)
     f (x, x') (xss, g) = let (xs, ng) = foldr (\x t -> let (ny, nng) = addNoise env (snd t) x nvs in (ny:fst t, nng)) ([], g) $ replicate 4 x ++ [x'] in (xs:xss, ng)
 
-fig12 :: Rand.StdGen -> [(Double, Int)]
-fig12 g = fst $ foldr (\iti t ->  let (xss, ng) = inputs env iti (snd t) nvs in ((learn env (initWeight++[lambda env]) xss (replicate 5 0.0) 0.0 0.0, iti) : fst t, ng)) ([], g) [3]
+fig12 :: Rand.StdGen -> [(Float, Int)]
+fig12 g = fst $ foldr (\iti t ->  let (xss, ng) = inputs env iti (snd t) nvs in ((learn env ws ws xss xs' 0.0 0.0, iti) : fst t, ng)) ([], g) [0..40]
   where
-    env = C { alfa = 0.9, beta = 0.0, gamma = 0.2, lambda = 0.6, mean = 0.005, deviation = 0.03}
+    env = C { alfa = 0.8885, beta = 0.0, gamma = 0.2, lambda = 0.6, mean = 0.005, deviation = 0.03}
     nvs = noiseValues
+    ws = initWeight++[lambda env]
+    xs' = replicate 5 0.0
+
+
+d :: NPD.T Float Float
+d = (NPD.shape $ NPS.normalCurve 0.005 0.03) [negate 100..100]
 
 debug :: Show a => a -> a
 debug a | T.trace (show a) False = undefined
